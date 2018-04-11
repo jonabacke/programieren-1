92191e66266418ba8485679b26d6967fa97f403a
johnny <jonathan.backes.jb@googlemail.com>
2018-04-11 22:40:12 +0200

    Kommentare

--- Blatt_3/Blatt03_3_Autorennen/Autorennen/Rennauto.java
+++ Blatt_3/Blatt03_3_Autorennen/Autorennen/Rennauto.java
@@ -1,19 +1,48 @@
-class Rennauto{
+/** In der Klasse Rennauto wird ein Auto erzeugt.
+  * Dieses Auto kann geradeaus und um die Kurve fahren.
+  * Beim um die Kurve fahren hat das Auto eine
+  * Maximalgeschwindigkeit von 60 m/s.
+  *
+  * @author Jonathan Backes, Linus Kurz
+  * @version 11.04.2018
+  *
+  */
+
+class Rennauto {
+
+  /**
+    * Feldvariablen
+    * @param _name Name des Fahrers
+    * @param _fahrzeugTyp Typ des Fahrzeugs
+    * @param _maxV Maximalgeschwindigkeit in mm/s
+    * @param _v_0 Geschwindigkeit, die für die Formel gebraucht wird
+    * @param _millimeterZahler gibt die bereits gefahrene Strecke in mm an
+    * @param _maxVKurve gibt an um wie viel v runter geht
+    * @param _v_Gerade Geschwindigkeit in der Kurve
+    * @param _v_Gerade Geschwindigkeit auf der Geraden
+    * @param _zeitK Zeit, die das Auto nach einer Kurve gefahren ist
+    * @param _zeitG Zeit, die das Auto nach einer Geraden gefahren ist
+    */
 
   String _name;
   String _fahrzeugTyp;
-  //@param _maxV in mm/s
   int _maxV;
   int _v_0;
   int _millimeterZaehler;
-  //@param _maxVKurve gibt an um wie viel v runter geht
   int _maxVKurve;
   int _v_Kurve;
   int _v_Gerade;
   int _zeitK;
   int _zeitG;
 
-  public Rennauto(String name, String fahrzeugTyp, int maxV){
+  /**
+    * Mit den Konstruktoren Rennauto wird ein Rennauto erzeugt
+    * @param name Den Namen des Fahrers eintragen maximal 10 Zeichen
+    * @param fahrzeugTyp Fahrzeugtyp eintragen
+    * @param maxV Maximalgeschwindigkeit eintragen
+    */
+
+  public Rennauto(String name, String fahrzeugTyp, int maxV) {
     _name = name;
     _fahrzeugTyp = fahrzeugTyp;
     _maxV = maxV;
@@ -24,7 +53,7 @@ class Rennauto{
     _zeitG = 0;
   }
 
-  public Rennauto(){
+  public Rennauto() {
     _name = "berta";
     _fahrzeugTyp = "fahrzeugTyp";
     _maxV = 100000;
@@ -35,75 +64,100 @@ class Rennauto{
     _zeitG = 0;
   }
 
-  public void fahreKurve(int anfangZeit, int endZeit){
+  /**
+      In der Methode fahreKurve wird das Auto auf 60 m/s gebremmst
+      die Geschwindigkeit wird mit der Gombertzfunktion berechnet
+      @param anfangZeit zeitpunkt vom Kurvenanfagn
+      @param endZeit zeitpunkt vom Kurvenende
+    */
+
+  public void fahreKurve(int anfangZeit, int endZeit) {
+    int gefahreneAbschnitt;
     _zeitK = endZeit;
     anfangZeit = anfangZeit - _zeitG;
     endZeit = endZeit - _zeitG;
-    int gefahreneAbschnitt;
-    for (int i = anfangZeit; i < endZeit; i++)
-    {
+    for (int i = anfangZeit; i < endZeit; i++) {
       //gefahreneAbschnitt / Zeiteinheit ( 1Sekunde)
       if (_v_Gerade > _maxVKurve) {
         gefahreneAbschnitt = gompertzFunktion((-_maxVKurve), 5, i, _v_Gerade);
-      }
-      else {
+      } else {
         gefahreneAbschnitt = gompertzFunktion((_maxVKurve), 5, i, _v_Gerade);
       }
       _millimeterZaehler += gefahreneAbschnitt;
-      print();
-      // System.out.println((double)(_millimeterZaehler)/1000+
-      // " Meter gefahren nach "+i+" Sekunden "
-      // +(double)(gefahreneAbschnitt)/1000);
       _v_Kurve = gefahreneAbschnitt;
+      print();
     }
   }
 
-  public void fahreGerade(int anfangZeit, int endZeit){
-    _zeitG = endZeit;
+  /**
+    *  In der Methode fahreGerade wird das Auto auf seine
+    *  Maiximaleschwindigkeit mit der Gombertzfunktion beschleunigt
+    *  @param anfangZeit zeitpunkt vom Geradenanfagn
+    *  @param endZeit zeitpunkt vom Geradenende
+    */
+
+  public void fahreGerade(int anfangZeit, int endZeit) {
     int gefahreneAbschnitt;
+    _zeitG = endZeit;
     anfangZeit = anfangZeit - _zeitK;
     endZeit = endZeit - _zeitK;
-    for (int i = anfangZeit; i < endZeit; i++)
-    {
+    for (int i = anfangZeit; i < endZeit; i++) {
       //gefahreneAbschnitt / Zeiteinheit ( 1Sekunde)
-      gefahreneAbschnitt = gompertzFunktion(_maxV-_v_Kurve, 5, i, _v_Kurve);
+      gefahreneAbschnitt = gompertzFunktion(_maxV - _v_Kurve, 5, i, _v_Kurve);
       _millimeterZaehler += gefahreneAbschnitt;
-      print();
-      // System.out.println((double)(_millimeterZaehler)/1000+
-      // " Meter gefahren nach "+i+" Sekunden "
-      // +(double)(gefahreneAbschnitt)/1000);
       _v_Gerade = gefahreneAbschnitt;
-
+      print();
     }
   }
 
-  //Benutzung der Gompertz-Funktion um ein stetigeres fahren zu simulieren
-  //@web https://de.wikipedia.org/wiki/Gompertz-Funktion
-  //@param faktor1 gibt die Asymptote an
-  //@param faktor2 gibt den stauchungsfaktor an
-  //@param laufvariable gibt den Zeitabschnitt an in dem wir uns befinden
-  //@param faktor3 gibt v_0 an
+  /**
+    * Benutzung der Gompertz-Funktion um ein stetigeres fahren zu simulieren
+    * @see https://de.wikipedia.org/wiki/Gompertz-Funktion
+    * @param faktor1 gibt die Asymptote an
+    * @param faktor2 gibt den stauchungsfaktor an
+    * @param laufvariable gibt den Zeitabschnitt an in dem wir uns befinden
+    * @param faktor3 gibt v_0 an also die Anfangsgeschwindigkeit
+    */
 
-
-  public int gompertzFunktion(int faktor1, int faktor2, int laufvariable, int faktor3){
+  public int gompertzFunktion(int faktor1, int faktor2, int laufvariable, int faktor3) {
     double b = 5;
-    double c = faktor2 * (Math.random()+1);
-    return (int) (faktor1*Math.exp(-b*Math.exp(-laufvariable/c))+faktor3);
+    double c = faktor2 * (Math.random() + 1);
+    return (int) (faktor1 * Math.exp(-b * Math.exp(-laufvariable / c)) + faktor3);
   }
 
-  public double gibStrecke(){
+  /**
+    * Mit der Methode gibStrecke wird die bisher
+    * gefahrenre Strecke zurückgegeben.
+    */
+
+  public int gibStrecke() {
     return _millimeterZaehler;
   }
 
-  public String gibName(){
+  /**
+    * Mit der Methode gibName wird der Name des
+    * Fahrers zurückgegeben.
+    */
+
+  public String gibName() {
     return _name;
   }
 
-  public int gibGefahreneStrecke(){
-    return _millimeterZaehler;
+  /**
+    * Mit der Methode print wird der Momentane
+    * Meterstand zurückgegeben.
+    */
+
+  public void print() {
+    System.out.printf("%10.3f", (double) (_millimeterZaehler) / 1000);
   }
 
-  public void print(){
-    System.out.printf("%10.3f",(double)(_millimeterZaehler)/1000);
+  /**
+    * Mit der Methode aufNullSetzen wird der
+    * Meterstand auf null gesetzt.
+    */
+
+  public void aufNullSetzen() {
+    _millimeterZaehler = 0;
   }
 }
--- Blatt_3/Blatt03_3_Autorennen/Autorennen/Rennbahn.java
+++ Blatt_3/Blatt03_3_Autorennen/Autorennen/Rennbahn.java
@@ -1,6 +1,26 @@
-class Rennbahn{
+/**
+ * In der Klasse Rennbahn wird eine Rennbahn erzeugt auf die bis zu vier
+ * Autos gesetzt werden können.
+ *
+ * @author Jonathan Backes, Linus Kurz
+ * @version 11.04.2018
+ */
 
-  //Streckenlänge in mm für höhere Genauigkeit
+class Rennbahn {
+
+  /**
+   * Hier werden die Feldvariablen erzeugt.
+   * Die Strecke wird in mm angegeben um eine höhere Genauigkeit zu bekommen
+   *
+   * @param _streckenlänge länge der Strecke die gefahren werden soll
+   * @param _auto1 erzeugung eines Rennautoexemplars, dass vorerst leer ist
+   * @param _auto2 erzeugung eines Rennautoexemplars, dass vorerst leer ist
+   * @param _auto3 erzeugung eines Rennautoexemplars, dass vorerst leer ist
+   * @param _auto4 erzeugung eines Rennautoexemplars, dass vorerst leer ist
+   * @param _auto1 erzeugung eines Rennautoexemplars, dass den spaeteren Sieger angibt
+   * @param _rennZeit gibt den Zeitverlauf des Rennens an
+   */
+
   int _streckenlänge;
   Rennauto _auto1;
   Rennauto _auto2;
@@ -9,30 +29,46 @@ class Rennbahn{
   Rennauto _sieger;
   int _rennZeit;
 
-  public Rennbahn(){
+  /**
+   * Im Konstruktor wird die Streckenlänge definiert
+   *
+   * @param streckenlänge Streckenlaenge in mm angeben.
+   */
+
+  public Rennbahn(int streckenlänge) {
+    _streckenlänge = streckenlänge;
+  }
+
+  public Rennbahn() {
     _streckenlänge = 10000000;
   }
 
+  /**
+   * Mit der Methode setzeAufSpur wird ein Auto auf die niedrigste Spur gesetzt
+   *
+   * @param auto das Auto welches auf eine Spur gesetzt werden soll
+   */
 
-  public void setzeAufSpur(Rennauto auto){
+  public void setzeAufSpur(Rennauto auto) {
     if (_auto1 == null) {
       _auto1 = auto;
-    }
-    else if (_auto2 == null) {
+    } else if (_auto2 == null) {
       _auto2 = auto;
-    }
-    else if (_auto3 == null) {
+    } else if (_auto3 == null) {
       _auto3 = auto;
-    }
-    else if (_auto4 == null) {
+    } else if (_auto4 == null) {
       _auto4 = auto;
-    }
-    else{
+    } else {
       System.out.println("Alle Bahnen sind belegt");
     }
   }
 
-  public void simuliereZeitabschnitt(int anfangZeit, int endZeit){
+  /**
+   * In der Methode simuliere Zeitabschnitt wird ein Zeitabschnitt zwischen
+   * @param anfangZeit und @param endZeit simuliert.
+   */
+
+  public void simuliereZeitabschnitt(int anfangZeit, int endZeit) {
     if (_auto1 != null) {
       fahre(_auto1, anfangZeit, endZeit);
     }
@@ -47,45 +83,57 @@ class Rennbahn{
     }
   }
 
-  public void fahre(Rennauto auto, int anfangZeit, int endZeit){
-    if (auto.gibGefahreneStrecke() % _streckenlänge < 1500000) {
+  /**
+     * In der Methode fahre wird fuer ein
+     * @param auto für
+     * @param anfangZeit
+     * und
+     * @param endZeit über die momentane zurueckgelegte Strecke
+     * ausgewaehlt ob es momentan um die Kurve faehrt oder gerade.
+     */
+
+  public void fahre(Rennauto auto, int anfangZeit, int endZeit) {
+    if (auto.gibStrecke() % _streckenlänge < 1500000) {
       auto.fahreGerade(anfangZeit, endZeit);
-    }
-    else if (auto.gibGefahreneStrecke() % _streckenlänge < 3500000) {
+    } else if (auto.gibStrecke() % _streckenlänge <= 3500000) {
       auto.fahreKurve(anfangZeit, endZeit);
-    }
-    else if (auto.gibGefahreneStrecke() % _streckenlänge < 6500000) {
+    } else if (auto.gibStrecke() % _streckenlänge <= 6500000) {
       auto.fahreGerade(anfangZeit, endZeit);
-    }
-    else if (auto.gibGefahreneStrecke() % _streckenlänge < 8500000) {
+    } else if (auto.gibStrecke() % _streckenlänge <= 8500000) {
       auto.fahreKurve(anfangZeit, endZeit);
-    }
-    else if (auto.gibGefahreneStrecke() % _streckenlänge < 10000000) {
+    } else if (auto.gibStrecke() % _streckenlänge <= 10000000) {
       auto.fahreGerade(anfangZeit, endZeit);
     }
   }
 
-  public boolean liefereSieger(){
+  /**
+   * In der Methode liefereSieger wird ermittelt ob es einen Sieger gibt
+   * und wenn ja wer dieser ist.
+   *
+   * @return ob es einen Sieger gibt
+   */
+
+  public boolean liefereSieger() {
     if (_auto1 != null) {
-      if(_streckenlänge-_auto1.gibStrecke() >= 0){
+      if ((_streckenlänge - _auto1.gibStrecke()) <= 0) {
         _sieger = _auto1;
         return true;
       }
     }
-    else if (_auto2 != null) {
-      if(_streckenlänge-_auto2.gibStrecke() >= 0){
+    if (_auto2 != null) {
+      if ((_streckenlänge - _auto2.gibStrecke()) <= 0) {
         _sieger = _auto2;
         return true;
       }
     }
-    else if (_auto3 != null) {
-      if(_streckenlänge-_auto3.gibStrecke() >= 0){
+    if (_auto3 != null) {
+      if ((_streckenlänge - _auto3.gibStrecke()) <= 0) {
         _sieger = _auto3;
         return true;
       }
     }
-    else if (_auto4 != null) {
-      if(_streckenlänge-_auto4.gibStrecke() >= 0){
+    if (_auto4 != null) {
+      if ((_streckenlänge - _auto4.gibStrecke()) <= 0) {
         _sieger = _auto4;
         return true;
       }
@@ -93,64 +141,92 @@ class Rennbahn{
     return false;
   }
 
-  public void rennenDurchführen(){
+  /**
+   * In der Methode rennenDurchführen wird das rennen
+   * durchgeführt bis es einen Sieger gibt.
+   */
 
-    if(_auto1 == null && _auto2 == null && _auto3 == null && _auto4 == null){
-        System.out.println("Kein Auto vorhanden");
+  public void rennenDurchfuehren() {
+    if (_auto1 == null && _auto2 == null && _auto3 == null && _auto4 == null) {
+      System.out.println("Kein Auto vorhanden");
     }
-
-    if (_auto4 != null) {
-      System.out.printf("|%10s||%10s||%10s||%10s|" ,_auto1.gibName(), _auto2.gibName(), _auto3.gibName(), _auto4.gibName());
-    }
-    else if (_auto3 != null) {
-      System.out.printf("|%10s||%10s||%10s|" ,_auto1.gibName(), _auto2.gibName(), _auto3.gibName());
-    }
-
-    else if (_auto2 != null) {
-      System.out.printf("|%10s||%10s|" ,_auto1.gibName(), _auto2.gibName());
-    }
-    else{
-      System.out.printf("|%10s|" ,_auto1.gibName());
-    }
-    System.out.println();
-    System.out.println("------------------------------------------------------");
-
+    ausgabenKopf();
     int zeit = 1;
-
-
-    do{
-
+    do {
       simuliereZeitabschnitt(zeit, zeit + 1);
       System.out.println();
       zeit++;
-    }
-    while(liefereSieger());
-
-    System.out.println(_sieger.gibName()+" ist Gewinner");
+    } while (!liefereSieger());
+    System.out.println(_sieger.gibName() + " ist Gewinner");
+    aufNullSetzen();
   }
 
-  public void entfernen(Rennauto auto){
+  /**
+   * Mit der Methode aufNullSetzen werden die gefahrenen Strecken aller Autos
+   * nach dem Rennen auf null gesetzt
+   */
+
+  public void aufNullSetzen() {
+    if (_auto1 != null) {
+      _auto1.aufNullSetzen();
+    }
+    if (_auto2 != null) {
+      _auto2.aufNullSetzen();
+    }
+    if (_auto3 != null) {
+      _auto3.aufNullSetzen();
+    }
+    if (_auto4 != null) {
+      _auto4.aufNullSetzen();
+    }
+  }
+
+  /**
+   * Mit der Methode entferne kann ein Auto von der Rennstrecke entfernt werden
+   *
+   * @param auto Auto welches entfernt werden soll
+   */
+
+  public void entfernen(Rennauto auto) {
     if (_auto1 == auto) {
       _auto1 = _auto2;
       _auto2 = _auto3;
       _auto3 = _auto4;
       _auto4 = null;
-    }
-    else if (_auto2 == auto) {
+    } else if (_auto2 == auto) {
       _auto2 = _auto3;
       _auto3 = _auto4;
-      _auto4 = null;;
-    }
-    else if (_auto3 == auto) {
-      _auto3 = _auto4;
-      _auto4 = null;;
-    }
-    else if (_auto4 == auto) {
       _auto4 = null;
-    }
-    else{
+      ;
+    } else if (_auto3 == auto) {
+      _auto3 = _auto4;
+      _auto4 = null;
+      ;
+    } else if (_auto4 == auto) {
+      _auto4 = null;
+    } else {
       System.out.println("Dieses Auto ist nicht vorhanden");
     }
   }
 
+  /**
+   * Diese Methode wird aufgerufen um in der Ausgabe die Fahrer oben anzugeben
+   */
+
+  public void ausgabenKopf() {
+    if (_auto4 != null) {
+      System.out.printf("|%10s||%10s||%10s||%10s|", _auto1.gibName(), _auto2.gibName(), _auto3.gibName(),
+          _auto4.gibName());
+    } else if (_auto3 != null) {
+      System.out.printf("|%10s||%10s||%10s|", _auto1.gibName(), _auto2.gibName(), _auto3.gibName());
+    }
+
+    else if (_auto2 != null) {
+      System.out.printf("|%10s||%10s|", _auto1.gibName(), _auto2.gibName());
+    } else {
+      System.out.printf("|%10s|", _auto1.gibName());
+    }
+    System.out.println();
+    System.out.println("------------------------------------------------------");
+  }
 }
